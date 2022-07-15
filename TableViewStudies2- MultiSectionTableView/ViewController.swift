b6be192a1ae64cf8942b83c8785e3969fdbb7dd4
buuraak77 <brkylmz8@hotmail.com>
2022-07-15 16:01:15 +0300

    Initial Commit

---
+++ TableViewStudies2- MultiSectionTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewStudies2- MultiSectionTableView
+//
+//  Created by Burak Yılmaz on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
